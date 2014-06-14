5c8d4d5372f35371a09752a51cda0edba7cc2b80
Michael Langford <michael.langford@rowdylabs.com>
2014-06-14 15:51:43 -0400

    Initial Commit

---
+++ CompilerBreaksWhenMakingPeople/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompilerBreaksWhenMakingPeople
+//
+//  Created by Michael Langford on 6/14/14.
+//  Copyright (c) 2014 Rowdy Labs LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
